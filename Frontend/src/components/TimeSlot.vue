f98c242b2378cbdf9f7d92450ea20b16ba4ec800
felixmertala <felixmer@student.chalmers.se>
2021-11-29 11:43:00 +0100

    Added basic timeslot without functionality

---
+++ Frontend/src/components/TimeSlot.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-list-group-item tabindex="0" @focus="handleFocus(slot._id)">
+    <p>{{ slot.dentist }} borra {{ slot.address }} {{ slot.time }}</p>
+    <b-button variant="danger" v-on:click="$emit('book-slot', slot._id)"
+      >BOKA</b-button
+    >
+  </b-list-group-item>
+</template>
+
+<script>
+export default {
+  name: 'slot-item',
+  props: ['slot'],
+  methods: {
+    handleFocus (_id) {
+      this.$emit('expand-slot', _id)
+    },
+    handleFocusOut () {
+      // do something here
+    }
+  }
+}
+</script>
---
+++ Frontend/src/pages/TimeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>AAAAAAAAA</p>
+    <b-list-group id="list" v-for="slot in slots" v-bind:key="slot.name">
+      <time-slot
+        v-on:expand-slot="expandSlot"
+        v-bind:slot="slot"
+        v-on:del-slot="deleteslot"
+      />
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+import TimeSlot from '../components/TimeSlot.vue'
+
+export default {
+  components: { TimeSlot },
+  name: 'TimeView',
+
+  mounted () {
+    let slot1 = { address: 'peepee town', dentist: 'pedro', time: '11:27' }
+    let slot2 = { address: 'peepee town', dentist: 'pedro', time: '11:29' }
+    let slots1 = []
+    slots1.push(slot1)
+    slots1.push(slot2)
+    this.slots = slots1
+    console.log('aaa')
+  },
+  data () {
+    return {
+      slots: [],
+      msg: 'Welcome o Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
