4638cc80091641d489dc441da8aec4487cc7243b
felixmertala <felixmer@student.chalmers.se>
2022-01-07 13:36:22 +0100

    dentist information

--- client/src/components/DentistMap.vue
+++ client/src/components/DentistMap.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="mapContainer" style="height: 500px">
+  <div id="mapContainer" style="height: 1080px">
     <l-map :zoom="zoom" :center="center" ref="map" id="map" style="width: 100%">
-      <l-marker :key="marker.id" v-for="marker in coordinates" :lat-lng="marker" @click="clickFunction()">
+      <l-marker :key="marker.data" v-for="marker in coordinates" :lat-lng="marker.location" @click="clickFunction(marker.data)">
         <l-popup id="customPopup"><div class="col d-flex justify-content-center">
     <dentist-card id="card" ></dentist-card></div></l-popup>
       </l-marker>
@@ -34,7 +34,9 @@ export default{
       center: [13.1367826, 77.5711133],
       bounds: null,
       coordinates: [],
-      showCard: false
+      clinics: [],
+      showCard: false,
+      selectedC: {}
     }
   },
   mounted() {
@@ -42,9 +44,12 @@ export default{
       .then((response) => {
         console.log(response.data.length)
         console.log(response)
+        this.clinics = response.data
         for (let i = 0; i < response.data.length; i++) {
-          this.coordinates.push(latLng(response.data[i].clinic.coordinate.latitude, response.data[i].clinic.coordinate.longitude))
-          console.log(this.coordinates[i])
+          let resp = response.data[i]
+          let clinicL = {location: latLng(resp.clinic.coordinate.latitude, resp.clinic.coordinate.longitude),
+            data: resp}
+          this.coordinates.push(clinicL)
         }
       })
       .catch((error) => {
@@ -60,8 +65,9 @@ export default{
     )
   },
   methods: {
-    clickFunction() {
+    clickFunction(data) {
       this.showCard = !this.showCard
+      this.selectedC = data
     }
 
   }
